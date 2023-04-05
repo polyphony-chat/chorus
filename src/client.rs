08d096e7635607e9932a30ab72d5a4073c14e547
bitfl0wer <florian@pro-weber.com>
2023-04-05 21:55:27 +0200

    Create files

---
+++ src/client.rs
@@ -0,0 +1,11 @@
+use crate::gateway::Gateway;
+use crate::http::Http;
+use crate::limit::Ratelimiter;
+
+struct ClientBuilder {}
+
+/* impl ClientBuilder {
+    fn build() -> Client {}
+} */
+
+struct Client {}
