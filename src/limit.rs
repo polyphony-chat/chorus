5fbea48b76459b9d9dd193d4c9120f626fb999db
bitfl0wer <florian@pro-weber.com>
2023-04-07 21:01:48 +0200

    start concepting/implementing RateLimiter

--- src/limit.rs
+++ src/limit.rs
@@ -1,1 +1,27 @@
-pub struct Ratelimiter {}
+use reqwest::{Client, Request};
+
+struct Limit {
+    limit: i64,
+    remaining: i64,
+    reset: i64,
+}
+
+pub struct Ratelimiter {
+    http: Client,
+    limit: Limit,
+    requests: Box<[Request]>, // wow, amazing
+}
+
+impl Ratelimiter {
+    pub fn new() -> Self {
+        Ratelimiter {
+            limit: Limit {
+                limit: 1,
+                remaining: 1,
+                reset: 0,
+            },
+            http: Client::new(),
+            requests: Box::new([]),
+        }
+    }
+}
