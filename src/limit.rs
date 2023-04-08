8d30e1461bf6d1a7e770005fd9eada4bee9d73a8
bitfl0wer <florian@pro-weber.com>
2023-04-08 14:37:20 +0200

    Use Vec<Limit> instead of Limit because there are multiple limits

--- src/limit.rs
+++ src/limit.rs
@@ -10,7 +10,7 @@ pub struct Limit {
 
 pub struct LimitedRequester {
     http: Client,
-    limit: Limit,
+    limit: Vec<Limit>,
     requests: VecDeque<Request>,
 }
 
@@ -19,27 +19,34 @@ impl LimitedRequester {
     /// send them to the server using a `Client`. It keeps track of the remaining requests that can
     /// be send within the `Limit` of an external API Ratelimiter, and looks at the returned request
     /// headers to see if it can find Ratelimit info to update itself.
-    pub fn new() -> Self {
+    pub fn new(api_url: String) -> Self {
         LimitedRequester {
-            limit: Limit {
-                limit: -1,
-                remaining: -1,
-                reset: -1,
-                bucket: String::new(),
-            },
+            limit: LimitedRequester::check_limits(api_url),
             http: Client::new(),
             requests: VecDeque::new(),
         }
     }
 
-    pub fn check_limits(url: String) -> Limit {
+    pub fn check_limits(url: String) -> Vec<Limit> {
         let client = Client::new();
-        let url_parsed = crate::URLBundle::parse_url(url);
-        Limit {
+        let url_parsed = crate::URLBundle::parse_url(url) + "/api/policies/instance/limits";
+        let mut limit_vector = Vec::new();
+        limit_vector.push(Limit {
             limit: -1,
             remaining: -1,
             reset: -1,
             bucket: String::new(),
-        } // TODO: Implement
+        }); // TODO: Implement
+        limit_vector
     }
 }
+
+/* #[cfg(test)]  Tests work here as well, neat!
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_url() {
+        assert_eq!(1, 1)
+    }
+} */
