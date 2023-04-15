ff60853d0b5246a2349ec7f49a7c96064939631a
bitfl0wer <florian@pro-weber.com>
2023-04-15 21:12:33 +0200

    create RegisterSchema

--- src/api/schemas.rs
+++ src/api/schemas.rs
@@ -1,1 +1,18 @@
-pub mod schemas {}
+pub mod schemas {
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Debug, Serialize, Deserialize)]
+    #[serde(rename_all = "snake_case")]
+    pub struct RegisterSchema {
+        username: Option<String>,
+        password: Option<String>,
+        consent: Option<bool>,
+        email: Option<String>,
+        fingerprint: Option<String>,
+        invite: Option<String>,
+        date_of_birth: Option<String>,
+        gift_code_sku_id: Option<String>,
+        captcha_key: Option<String>,
+        promotional_email_opt_in: Option<bool>,
+    }
+}
