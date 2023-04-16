3be962146b5891c029f23928b1e102fc3553bc98
bitfl0wer <florian@pro-weber.com>
2023-04-16 23:03:12 +0200

    add gateway object

--- src/instance.rs
+++ src/instance.rs
@@ -6,9 +6,17 @@ use crate::URLBundle;
 use std::collections::HashMap;
 
 #[derive(Debug)]
+/**
+The [`Instance`] what you will be using to perform all sorts of actions on the Spacebar server.
+ */
 pub struct Instance {
     main_url: String,
     urls: URLBundle,
     instance_info: InstancePoliciesSchema,
     requester: LimitedRequester,
+    gateway: Gateway,
+}
+
+impl Instance {
+    pub fn new() {}
 }
