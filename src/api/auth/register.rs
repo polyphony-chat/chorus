fec52f506d8b90fbf977d340d1b2b3cc8deb93b7
bitfl0wer <florian@pro-weber.com>
2023-04-20 00:14:11 +0200

    begin working on register

--- src/api/auth/register.rs
+++ src/api/auth/register.rs
@@ -1,7 +1,34 @@
 pub mod register {
-    use crate::instance::Instance;
+    use std::fmt;
+
+    use reqwest::Client;
+
+    use crate::{api::schemas::schemas::RegisterSchema, instance::Instance};
 
     impl Instance {
-        pub fn register() {}
+        pub fn register(&mut self, register_schema: &RegisterSchema) {
+            let limited_requester = &self.requester;
+            let client = Client::new();
+            let endpoint_url = &self.urls.get_api() + "/"
+        }
     }
+
+    #[derive(Debug, PartialEq, Eq)]
+    pub struct RegisterError {
+        pub message: String,
+    }
+
+    impl RegisterError {
+        fn new(message: String) -> Self {
+            RegisterError { message }
+        }
+    }
+
+    impl fmt::Display for RegisterError {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "{}", self.message)
+        }
+    }
+
+    impl std::error::Error for RegisterError {}
 }
