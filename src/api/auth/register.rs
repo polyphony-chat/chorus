d98316b3eea38e00551f046ff1c02ff28ed22c50
bitfl0wer <florian@pro-weber.com>
2023-04-21 14:27:56 +0200

    Fix bad build

--- src/api/auth/register.rs
+++ src/api/auth/register.rs
@@ -2,14 +2,17 @@ pub mod register {
     use std::fmt;
 
     use reqwest::Client;
+    use serde_json::json;
 
     use crate::{api::schemas::schemas::RegisterSchema, instance::Instance};
 
     impl Instance {
         pub fn register(&mut self, register_schema: &RegisterSchema) {
+            let json_schema = json!(register_schema);
             let limited_requester = &self.requester;
             let client = Client::new();
-            let endpoint_url = &self.urls.get_api() + "/"
+            let endpoint_url = self.urls.get_api().to_string() + "/auth/register";
+            let request_builder = client.post(endpoint_url).body(json_schema.to_string());
         }
     }
 
