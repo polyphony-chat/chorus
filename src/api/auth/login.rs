22adda7aa65b8097da4541fd46a5ead7949a317f
bitfl0wer <florian@pro-weber.com>
2023-04-23 14:00:27 +0200

    start working on login

--- src/api/auth/login.rs
+++ src/api/auth/login.rs
@@ -1,8 +1,13 @@
 pub mod login {
-    use crate::api::schemas::schemas::LoginSchema;
+    use crate::api::schemas::schemas::{LoginResult, LoginSchema};
+    use crate::errors::InstanceServerError;
     use crate::instance::Instance;
 
     impl Instance {
-        pub async fn login_account(&mut self, login_schema: &LoginSchema) {}
+        pub async fn login_account(
+            &mut self,
+            login_schema: &LoginSchema,
+        ) -> Result<LoginResult, InstanceServerError> {
+        }
     }
 }
