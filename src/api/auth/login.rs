242eddd5c8e522d4ea0d38d79f4cdbad76fb954f
bitfl0wer <florian@pro-weber.com>
2023-04-23 14:01:02 +0200

    make compiler happy

--- src/api/auth/login.rs
+++ src/api/auth/login.rs
@@ -3,11 +3,11 @@ pub mod login {
     use crate::errors::InstanceServerError;
     use crate::instance::Instance;
 
-    impl Instance {
+    /* impl Instance {
         pub async fn login_account(
             &mut self,
             login_schema: &LoginSchema,
         ) -> Result<LoginResult, InstanceServerError> {
         }
-    }
+    } */
 }
