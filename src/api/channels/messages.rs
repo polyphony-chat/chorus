7938addf67b2466e4a32fc9b06e07295521f0a30
bitfl0wer <florian@pro-weber.com>
2023-05-03 15:55:01 +0200

    Start working on message send body

--- src/api/channels/messages.rs
+++ src/api/channels/messages.rs
@@ -1,6 +1,24 @@
 pub mod messages {
+    use reqwest::Client;
+    use serde_json::to_string;
+
+    use crate::api::limits::Limits;
     use crate::api::types::Message;
     use crate::instance::Instance;
+    use crate::limit::LimitedRequester;
 
-    impl Message {}
+    impl Message {
+        pub async fn send(
+            url_api: &String,
+            message: &Message,
+            limits: &Limits,
+            requester: &mut LimitedRequester,
+        ) {
+            let client = Client::new()
+                .post(url_api)
+                .body(to_string(message).unwrap())
+                .send()
+                .await;
+        }
+    }
 }
