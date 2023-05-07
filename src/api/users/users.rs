7904b3d9f88fb04a4c20ad2b861b89812714855c
bitfl0wer <florian@pro-weber.com>
2023-05-07 11:54:49 +0200

    add docs, add instance.get_user()

--- src/api/users/users.rs
+++ src/api/users/users.rs
@@ -4,9 +4,20 @@ use crate::{
         types::{User, UserObject},
     },
     errors::InstanceServerError,
+    instance::Instance,
 };
 
 impl<'a> User<'a> {
+    /**
+    Get a user object by id, or get the current user.
+    # Arguments
+    * `token` - A valid access token for the API.
+    * `url_api` - The URL to the API.
+    * `id` - The id of the user that will be retrieved. If this is None, the current user will be retrieved.
+    * `instance_limits` - The [`Limits`] of the instance.
+    # Errors
+    * [`InstanceServerError`] - If the request fails.
+     */
     pub async fn get(
         token: &String,
         url_api: &String,
@@ -33,5 +44,31 @@ impl<'a> User<'a> {
             Ok(result) => Ok(serde_json::from_str(&result.text().await.unwrap()).unwrap()),
             Err(e) => Err(e),
         }
+    }
+}
+
+impl<'a> Instance<'a> {
+    /**
+    Get a user object by id, or get the current user.
+    # Arguments
+    * `token` - A valid access token for the API.
+    * `id` - The id of the user that will be retrieved. If this is None, the current user will be retrieved.
+    # Errors
+    * [`InstanceServerError`] - If the request fails.
+    # Notes
+    This function is a wrapper around [`User::get`].
+     */
+    pub async fn get_user(
+        &mut self,
+        token: String,
+        id: Option<&String>,
+    ) -> Result<UserObject, InstanceServerError> {
+        User::get(
+            &token,
+            &self.urls.get_api().to_string(),
+            id,
+            &mut self.limits,
+        )
+        .await
     }
 }
