e552bdb352480cf4236a27e22560bad29324fcf1
bitfl0wer <florian@pro-weber.com>
2023-05-14 22:45:55 +0200

    make fields on GuildCreateSchema pub

--- src/api/schemas.rs
+++ src/api/schemas.rs
@@ -290,13 +290,13 @@ impl MessageSendSchema {
 #[derive(Debug, Deserialize, Serialize)]
 #[serde(rename_all = "snake_case")]
 pub struct GuildCreateSchema {
-    name: Option<String>,
-    region: Option<String>,
-    icon: Option<String>,
-    channels: Option<Vec<Channel>>,
-    guild_template_code: Option<String>,
-    system_channel_id: Option<String>,
-    rules_channel_id: Option<String>,
+    pub name: Option<String>,
+    pub region: Option<String>,
+    pub icon: Option<String>,
+    pub channels: Option<Vec<Channel>>,
+    pub guild_template_code: Option<String>,
+    pub system_channel_id: Option<String>,
+    pub rules_channel_id: Option<String>,
 }
 
 // I know that some of these tests are... really really basic and unneccessary, but sometimes, I
