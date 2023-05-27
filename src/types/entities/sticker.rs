1e2587d86600b290e3cc17648ec290f7f14cbdb1
Zertex <me@zertex.dev>
2023-05-26 22:53:33 -0400

    Add StickerType and StickerFormat

--- src/types/entities/sticker.rs
+++ src/types/entities/sticker.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use crate::types::{entities::User, utils::Snowflake};
 
@@ -12,8 +13,8 @@ pub struct Sticker {
     pub tags: String,
     pub asset: Option<String>,
     #[serde(rename = "type")]
-    pub sticker_type: u8,
-    pub format_type: u8,
+    pub sticker_type: StickerType,
+    pub format_type: StickerFormat,
     pub available: Option<bool>,
     pub guild_id: Option<Snowflake>,
     #[cfg_attr(feature = "sqlx", sqlx(skip))]
@@ -25,5 +26,23 @@ pub struct Sticker {
 pub struct StickerItem {
     pub id: Snowflake,
     pub name: String,
-    pub format_type: u8,
+    pub format_type: StickerFormat,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Serialize_repr, Deserialize_repr)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+#[repr(i32)]
+pub enum StickerType {
+    Standard = 1,
+    Guild = 2,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Serialize_repr, Deserialize_repr)]
+#[cfg_attr(feature = "sqlx", derive(sqlx::Type))]
+#[repr(i32)]
+pub enum StickerFormat {
+    Png = 1,
+    Apng = 2,
+    Lottie = 3,
+    Gif = 4,
 }
