0958e1e63b3c7272d7f8c4a05d917fc825297367
Quaternion <quat3rnion@proton.me>
2023-05-27 00:24:37 -0400

    Revert "Add back settings and extended_settings fields to User"
    This reverts commit 04813967c24f61ceb0083d6bdc8aa8bdd7670a64.

--- src/types/entities/user.rs
+++ src/types/entities/user.rs
@@ -49,12 +49,6 @@ pub struct User {
     pub purchased_flags: i32,
     pub premium_usage_flags: i32,
     pub disabled: Option<bool>,
-    #[cfg_attr(feature = "sqlx", sqlx(skip))]
-    pub settings: UserSettings,
-    #[cfg(feature = "sqlx")]
-    pub extended_settings: sqlx::types::Json<Value>,
-    #[cfg(not(feature = "sqlx"))]
-    pub extended_settings: Value,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
