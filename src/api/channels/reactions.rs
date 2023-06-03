df6fcbd5ee2c46e383c3fe223120a9b092736a60
bitfl0wer <florian@pro-weber.com>
2023-06-03 15:21:49 +0200

    Add ReactionMeta struct

--- src/api/channels/reactions.rs
+++ src/api/channels/reactions.rs
@@ -1,1 +1,9 @@
 use crate::types;
+
+/**
+Extends the [`types::Reaction`] struct with useful metadata.
+ */
+pub struct ReactionMeta {
+    pub message_id: types::Snowflake,
+    pub reaction: types::Reaction,
+}
