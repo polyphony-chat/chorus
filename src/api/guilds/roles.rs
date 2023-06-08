f41312d430345df3f6a2d2f912caa9095f7b823d
bitfl0wer <florian@pro-weber.com>
2023-06-08 22:26:24 +0200

    Implement RoleObject::create()

--- src/api/guilds/roles.rs
+++ src/api/guilds/roles.rs
@@ -2,6 +2,7 @@ use reqwest::Client;
 use serde_json::{from_str, to_string};
 
 use crate::{
+    errors::ChorusLibError,
     instance::UserMeta,
     limit::LimitedRequester,
     types::{self, RoleCreateModifySchema, RoleObject},
@@ -41,13 +42,39 @@ impl types::RoleObject {
         user: &mut UserMeta,
         guild_id: &str,
         role_create_schema: RoleCreateModifySchema,
-    ) {
+    ) -> Result<RoleObject, ChorusLibError> {
         let mut belongs_to = user.belongs_to.borrow_mut();
         let url = format!("{}/guilds/{}/roles/", belongs_to.urls.get_api(), guild_id);
         let body = match to_string::<RoleCreateModifySchema>(&role_create_schema) {
             Ok(string) => string,
-            Err(e) => 
+            Err(e) => {
+                return Err(ChorusLibError::FormCreationError {
+                    error: e.to_string(),
+                })
+            }
         };
-        let request = Client::new().post(url).bearer_auth(user.token()).body()
+        let request = Client::new().post(url).bearer_auth(user.token()).body(body);
+        let result = match LimitedRequester::new()
+            .await
+            .send_request(
+                request,
+                crate::api::limits::LimitType::Guild,
+                &mut belongs_to.limits,
+                &mut user.limits,
+            )
+            .await
+        {
+            Ok(request) => request,
+            Err(e) => return Err(e),
+        };
+        let role: RoleObject = match from_str(&result.text().await.unwrap()) {
+            Ok(role) => role,
+            Err(e) => {
+                return Err(ChorusLibError::InvalidResponseError {
+                    error: e.to_string(),
+                })
+            }
+        };
+        Ok(role)
     }
 }
