46fac4b132e1138e15c3ac7978b47ac162acc39e
bitfl0wer <florian@pro-weber.com>
2023-06-08 20:40:36 +0200

    Add RoleCreateSchema

--- src/types/schema/guild.rs
+++ src/types/schema/guild.rs
@@ -12,3 +12,15 @@ pub struct GuildCreateSchema {
     pub system_channel_id: Option<String>,
     pub rules_channel_id: Option<String>,
 }
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub struct RoleCreateSchema {
+    pub name: Option<String>,
+    pub permissions: Option<String>,
+    pub color: Option<u32>,
+    pub hoist: Option<bool>,
+    pub icon: Option<Vec<u8>>,
+    pub unicode_emoji: Option<String>,
+    pub mentionable: Option<bool>,
+}
