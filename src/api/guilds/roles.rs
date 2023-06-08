47f8b131a3303d8946e09aa5fd74ac85485a51d5
bitfl0wer <florian@pro-weber.com>
2023-06-08 22:39:06 +0200

    Add documentation

--- src/api/guilds/roles.rs
+++ src/api/guilds/roles.rs
@@ -9,6 +9,20 @@ use crate::{
 };
 
 impl types::RoleObject {
+    /// Retrieves all roles for a given guild.
+    ///
+    /// # Arguments
+    ///
+    /// * `user` - A mutable reference to a [`UserMeta`] instance.
+    /// * `guild_id` - The ID of the guild to retrieve roles from.
+    ///
+    /// # Returns
+    ///
+    /// An `Option` containing a `Vec` of [`RoleObject`]s if roles were found, or `None` if no roles were found.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`ChorusLibError`] if the request fails or if the response is invalid.
     pub async fn get_all(
         user: &mut UserMeta,
         guild_id: &str,
@@ -38,6 +52,21 @@ impl types::RoleObject {
         Ok(Some(roles))
     }
 
+    /// Creates a new role for a given guild.
+    ///
+    /// # Arguments
+    ///
+    /// * `user` - A mutable reference to a [`UserMeta`] instance.
+    /// * `guild_id` - The ID of the guild to create the role in.
+    /// * `role_create_schema` - A [`RoleCreateModifySchema`] instance containing the properties of the role to be created.
+    ///
+    /// # Returns
+    ///
+    /// A `Result` containing the newly created [`RoleObject`] if successful, or a [`ChorusLibError`] if the request fails or if the response is invalid.
+    ///
+    /// # Errors
+    ///
+    /// Returns a [`ChorusLibError`] if the request fails or if the response is invalid.
     pub async fn create(
         user: &mut UserMeta,
         guild_id: &str,
