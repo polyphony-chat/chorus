a90595c176ee7d0bc33c962bc4171e43cbd27d64
Flori Weber <florian@pro-weber.com>
2023-06-10 17:35:09 +0200

    Complete this test

--- tests/member.rs
+++ tests/member.rs
@@ -7,7 +7,32 @@ async fn add_remove_role() {
     let role_id = &bundle.role.id.to_string();
     let user_id = &bundle.user.object.id.to_string();
     chorus::types::GuildMember::add_role(&mut bundle.user, guild_id, user_id, role_id).await;
+    let member = chorus::types::GuildMember::get(&mut bundle.user, &guild_id, &user_id)
+        .await
+        .unwrap();
+    let mut role_found = false;
+    for role in member.roles.iter() {
+        if role == role_id {
+            println!("Role found: {:?}", role);
+            role_found = true;
+        }
+    }
+    if !role_found {
+        assert!(false)
+    }
     chorus::types::GuildMember::remove_role(&mut bundle.user, guild_id, user_id, role_id).await;
-    // TODO: Implement /guilds/{guild_id}/members/{member_id}/ GET route.
+    let member = chorus::types::GuildMember::get(&mut bundle.user, &guild_id, &user_id)
+        .await
+        .unwrap();
+    for role in member.roles.iter() {
+        if role != role_id {
+            role_found = false;
+        } else {
+            assert!(false);
+        }
+    }
+    if role_found {
+        assert!(false)
+    }
     common::teardown(bundle).await
 }
