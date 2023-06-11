1dbc05d48ce305aeca962e13c5a4f349310465e6
Flori Weber <florian@pro-weber.com>
2023-06-11 15:34:05 +0200

    create common.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,10 +1,12 @@
-pub mod auth;
-pub mod channels;
-pub mod guilds;
-pub mod policies;
-pub mod users;
-
 pub use channels::messages::*;
+pub use common::*;
 pub use guilds::*;
 pub use policies::instance::instance::*;
 pub use policies::instance::limits::*;
+
+pub mod auth;
+pub mod channels;
+pub mod common;
+pub mod guilds;
+pub mod policies;
+pub mod users;
