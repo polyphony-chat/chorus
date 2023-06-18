3bf911745fd3be0a40cdb799cdd6b63ac5f6c9a9
Flori Weber <florian@pro-weber.com>
2023-06-18 13:59:11 +0200

    Send a friend request

--- tests/relationships.rs
+++ tests/relationships.rs
@@ -21,7 +21,12 @@ async fn test_get_mutual_relationships() {
     let bundle = common::setup().await;
     let mut belongs_to = bundle.instance;
     let mut user = bundle.user;
-    let other_user = belongs_to.register_account(&register_schema).await.unwrap();
+    let mut other_user = belongs_to.register_account(&register_schema).await.unwrap();
+    let friend_request_schema = types::FriendRequestSendSchema {
+        username: user.object.username.clone(),
+        discriminator: Some(user.object.discriminator.clone()),
+    };
+    other_user.send_friend_request(friend_request_schema).await;
     let relationships = user
         .get_mutual_relationships(&other_user.object.id.to_string())
         .await
