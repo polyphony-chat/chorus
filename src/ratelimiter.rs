1d39f3ba19992c9bdcd12160a5964d7ce4bdbcad
Flori Weber <florian@pro-weber.com>
2023-06-23 20:32:12 +0200

    Add missing logic to send_request

--- src/ratelimiter.rs
+++ src/ratelimiter.rs
@@ -15,8 +15,8 @@ pub struct ChorusRequest {
 impl ChorusRequest {
     pub async fn send_request(self, user: &mut UserMeta) -> ChorusResult<Response> {
         let belongs_to = user.belongs_to.borrow();
-        if (belongs_to.limits.is_some()
-            && !ChorusRequest::can_send_request(belongs_to.limits.unwrap(), &self.limit_type))
+        if belongs_to.limits.is_some()
+            && !ChorusRequest::can_send_request(belongs_to.limits.unwrap(), &self.limit_type)
         {
             return Err(ChorusLibError::RateLimited {
                 bucket: format!("{:?}", self.limit_type),
@@ -28,12 +28,18 @@ impl ChorusRequest {
             .await
         {
             Ok(result) => result,
-            Err(e) => return Err(ChorusLibError::ReceivedErrorCodeError { error_code: e }),
+            Err(e) => {
+                return Err(ChorusLibError::ReceivedErrorCodeError {
+                    error_code: e.to_string(),
+                })
+            }
         };
         drop(belongs_to);
         if (!result.status().is_success()) {
-            Err(ChorusRequest::interpret_error(http_error))
+            return Err(ChorusRequest::interpret_error(result));
         }
+        ChorusRequest::update_rate_limits(user, &self.limit_type);
+        Ok(result)
     }
 
     fn can_send_request(ratelimits: &Ratelimits, limit_type: &LimitType) -> bool {}
