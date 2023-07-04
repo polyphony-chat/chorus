1dc9719501bf220a39c60d53e562a5c4114b6c19
Flori Weber <florian@pro-weber.com>
2023-07-04 21:54:52 +0200

    add ratelimits.to_hash_map()

--- src/types/config/types/limit_configuration.rs
+++ src/types/config/types/limit_configuration.rs
@@ -1,9 +1,17 @@
-use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, hash::Hash};
 
-use crate::types::config::types::subconfigs::limits::{
-    channel::ChannelLimits, global::GlobalRateLimits, guild::GuildLimits, message::MessageLimits,
-    rates::RateLimits, user::UserLimits,
+use serde::{Deserialize, Serialize};
+use strum::IntoEnumIterator;
+
+use crate::{
+    api::limits::LimitType,
+    types::config::types::subconfigs::limits::{
+        channel::ChannelLimits, global::GlobalRateLimits, guild::GuildLimits,
+        message::MessageLimits, rates::RateLimits, user::UserLimits,
+    },
 };
+
+use super::subconfigs::limits::ratelimits::RateLimitOptions;
 
 #[derive(Debug, Clone, Default, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
@@ -14,4 +22,19 @@ pub struct LimitsConfiguration {
     pub channel: ChannelLimits,
     pub rate: RateLimits,
     pub absolute_rate: GlobalRateLimits,
+}
+
+impl RateLimits {
+    pub fn to_hash_map(&self) -> HashMap<LimitType, RateLimitOptions> {
+        let mut map = HashMap::new();
+        map.insert(LimitType::AuthLogin, self.routes.auth.login);
+        map.insert(LimitType::AuthRegister, self.routes.auth.register);
+        map.insert(LimitType::Channel, self.routes.channel);
+        map.insert(LimitType::Error, self.error);
+        map.insert(LimitType::Global, self.global);
+        map.insert(LimitType::Ip, self.ip);
+        map.insert(LimitType::Webhook, self.routes.webhook);
+        map.insert(LimitType::Guild, self.routes.guild);
+        map
+    }
 }
