b70549217c0d04ee2ebab353a7e31fbb1bef0ae0
Flori Weber <florian@pro-weber.com>
2023-07-09 18:08:31 +0200

    Add more doc comments

--- src/api/policies/instance/ratelimits.rs
+++ src/api/policies/instance/ratelimits.rs
@@ -4,6 +4,7 @@ use crate::types::Snowflake;
 
 /// The different types of ratelimits that can be applied to a request. Includes "Baseline"-variants
 /// for when the Snowflake is not yet known.
+/// See <https://discord.com/developers/docs/topics/rate-limits#rate-limits> for more information.
 #[derive(Clone, Copy, Eq, PartialEq, Debug, Default, Hash)]
 pub enum LimitType {
     AuthRegister,
@@ -23,6 +24,7 @@ pub enum LimitType {
 /// A struct that represents the current ratelimits, either instance-wide or user-wide.
 /// Unlike [`RateLimits`], this struct shows the current ratelimits, not the rate limit
 /// configuration for the instance.
+/// See <https://discord.com/developers/docs/topics/rate-limits#rate-limits> for more information.
 #[derive(Debug, Clone)]
 pub struct Limit {
     pub bucket: LimitType,
