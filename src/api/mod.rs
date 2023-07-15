955aeb9292ab7e52165663285d0754cd9e005c07
bitfl0wer <florian@pro-weber.com>
2023-07-15 21:06:22 +0200

    Create api/invites/mod.rs

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,5 +1,6 @@
 pub use channels::messages::*;
 pub use guilds::*;
+pub use invites::*;
 pub use policies::instance::instance::*;
 pub use policies::instance::ratelimits::*;
 pub use users::*;
@@ -7,5 +8,6 @@ pub use users::*;
 pub mod auth;
 pub mod channels;
 pub mod guilds;
+pub mod invites;
 pub mod policies;
 pub mod users;
