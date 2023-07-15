25f8941822f743a40814cdd746185fbd1d504efb
bitfl0wer <florian@pro-weber.com>
2023-07-16 01:13:14 +0200

    start working on invite tests

--- tests/invites.rs
+++ tests/invites.rs
@@ -1,1 +1,10 @@
+mod common;
 
+#[tokio::test]
+async fn create_accept_invite() {
+    let mut bundle = common::setup().await;
+    let user = &mut bundle.user;
+    let invite = user.create_user_invite(None).await.unwrap();
+    let mut other_user = bundle.create_user("testuser1312").await;
+    other_user.accept_invite(&invite.code, None).await.unwrap();
+}
