310025e90f8556f372e54187b6fb9ed01216fc41
bitfl0wer <florian@pro-weber.com>
2023-07-17 00:13:16 +0200

    Create CreateChannelInviteSchema and friends

--- src/types/schema/channel.rs
+++ src/types/schema/channel.rs
@@ -1,3 +1,4 @@
+use bitflags::bitflags;
 use serde::{Deserialize, Serialize};
 
 use crate::types::{entities::PermissionOverwrite, Snowflake};
@@ -93,4 +94,33 @@ impl GetChannelMessagesSchema {
             ..self
         }
     }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+pub struct CreateChannelInviteSchema {
+    pub flags: Option<InviteFlags>,
+    pub max_age: u32,
+    pub max_uses: u8,
+    pub temporary: bool,
+    pub unique: bool,
+    pub validate: String,
+    pub target_type: InviteType,
+    pub target_user_id: Snowflake,
+    pub target_application_id: Snowflake,
+}
+
+bitflags! {
+    #[derive(Debug, Default, Clone, PartialEq, Eq, Serialize, Deserialize)]
+    pub struct InviteFlags: u64 {
+        const GUEST = 1 << 0;
+    }
+}
+
+#[derive(Debug, Deserialize, Serialize, Clone)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum InviteType {
+    Stream = 1,
+    EmbeddedApplication = 2,
+    RoleSubscriptions = 3,
+    CreatorPage = 4,
 }
