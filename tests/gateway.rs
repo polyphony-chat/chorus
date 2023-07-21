ccf44a53757485de4f8242d105671047ead5e913
Flori <39242991+bitfl0wer@users.noreply.github.com>
2023-07-21 15:35:31 +0200

    Auto updating structs (#163)
    * Gateway fields don't need to be pub
    
    * Add store to Gateway
    
    * Add UpdateMessage trait
    
    * Proof of concept: impl UpdateMessage for Channel
    
    * Start working on auto updating structs
    
    * Send entity updates over watch channel
    
    * Add id to UpdateMessage
    
    * Create trait Updateable
    
    * Add documentation
    
    * add gateway test
    
    * Complete test
    
    * Impl UpdateMessage::update() for ChannelUpdate
    
    * Impl UpdateMessage::update() for ChannelUpdate
    Co-authored by: SpecificProtagonist <specificprotagonist@posteo.org>
    
    * make channel::modify no longer mutate Channel
    
    * change modify call
    
    * remove unused imports
    
    * Allow dead code with TODO to remove it
    
    * fix channel::modify test
    
    * Update src/gateway.rs
    
    Co-authored-by: SpecificProtagonist <vincentjunge@posteo.net>
    
    ---------
    
    Co-authored-by: SpecificProtagonist <vincentjunge@posteo.net>

--- tests/gateway.rs
+++ tests/gateway.rs
@@ -1,6 +1,7 @@
 mod common;
+
 use chorus::gateway::*;
-use chorus::types;
+use chorus::types::{self, Channel};
 
 #[tokio::test]
 /// Tests establishing a connection (hello and heartbeats) on the local gateway;
@@ -21,4 +22,27 @@ async fn test_gateway_authenticate() {
     identify.token = bundle.user.token;
 
     gateway.send_identify(identify).await;
+}
+
+#[tokio::test]
+async fn test_self_updating_structs() {
+    let mut bundle = common::setup().await;
+    let gateway = Gateway::new(bundle.urls.wss).await.unwrap();
+    let mut identify = types::GatewayIdentifyPayload::common();
+    identify.token = bundle.user.token.clone();
+    gateway.send_identify(identify).await;
+    let channel_receiver = gateway.observe(bundle.channel.clone()).await;
+    let received_channel = channel_receiver.borrow();
+    assert_eq!(*received_channel, bundle.channel);
+    drop(received_channel);
+    let channel = &mut bundle.channel;
+    let modify_data = types::ChannelModifySchema {
+        name: Some("beepboop".to_string()),
+        ..Default::default()
+    };
+    Channel::modify(channel, modify_data, channel.id, &mut bundle.user)
+        .await
+        .unwrap();
+    let received_channel = channel_receiver.borrow();
+    assert_eq!(received_channel.name.as_ref().unwrap(), "beepboop");
 }
