99fed46225067acbf40c9c0b1ec0adb4b57d698f
bitfl0wer <florian@pro-weber.com>
2023-08-20 19:26:00 +0200

    Make ban test a little more meaningful

--- tests/guilds.rs
+++ tests/guilds.rs
@@ -56,5 +56,13 @@ async fn guild_create_ban() {
     )
     .await
     .unwrap();
+    assert!(Guild::create_ban(
+        guild.id,
+        other_user_id,
+        GuildBanCreateSchema::default(),
+        &mut bundle.user,
+    )
+    .await
+    .is_err());
     common::teardown(bundle).await
 }
