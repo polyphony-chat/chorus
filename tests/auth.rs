c6fa385e2822b39f04f55ca34aff1f89271607ac
bitfl0wer <florian@pro-weber.com>
2023-09-01 15:02:44 +0200

    Fix auth

--- tests/auth.rs
+++ tests/auth.rs
@@ -4,13 +4,13 @@ mod common;
 
 #[tokio::test]
 async fn test_registration() {
-    let mut bundle = common::setup().await;
+    let bundle = common::setup().await;
     let reg = RegisterSchema {
         username: "Hiiii".into(),
         date_of_birth: Some("2000-01-01".to_string()),
         consent: true,
         ..Default::default()
     };
-    bundle.instance.register_account(reg).await.unwrap();
+    bundle.instance.clone().register_account(reg).await.unwrap();
     common::teardown(bundle).await;
 }
