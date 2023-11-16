cefaa6545f7a96020a74514841843fdea0ee4b0e
bitfl0wer <florian@pro-weber.com>
2023-11-16 19:25:55 +0100

    Remove duplicate code

--- src/gateway/wasm/heartbeat.rs
+++ src/gateway/wasm/heartbeat.rs
@@ -2,15 +2,3 @@ use tokio::task::JoinHandle;
 use ws_stream_wasm::*;
 
 use super::*;
-
-/// Handles sending heartbeats to the gateway in another thread
-#[allow(dead_code)] // FIXME: Remove this, once used
-#[derive(Debug)]
-pub struct WasmHeartbeatHandler {
-    /// How ofter heartbeats need to be sent at a minimum
-    pub heartbeat_interval: Duration,
-    /// The send channel for the heartbeat thread
-    pub send: Sender<HeartbeatThreadCommunication>,
-    /// The handle of the thread
-    handle: JoinHandle<()>,
-}
