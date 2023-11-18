6ae715988d0e08dbe3f0db491f5da89427eee905
bitfl0wer <florian@pro-weber.com>
2023-11-18 20:02:45 +0100

    Refactor WasmGateway struct and import statements

--- src/gateway/wasm/gateway.rs
+++ src/gateway/wasm/gateway.rs
@@ -1,6 +1,16 @@
-pub use super::*;
+use std::sync::Arc;
+
+use super::events::Events;
+use super::*;
 use ws_stream_wasm::*;
+
+use crate::types::{self, WebSocketEvent};
 #[derive(Debug)]
 pub struct WasmGateway {
     events: Arc<Mutex<Events>>,
+    heartbeat_handler: HeartbeatHandler,
+    websocket_send: Arc<Mutex<SplitSink<WsStream, WsMessage>>>,
+    websocket_receive: SplitStream<WsStream>,
+    store: GatewayStore,
+    url: String,
 }
