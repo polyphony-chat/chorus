a0d14ceaf9910f501ac7dc62b5bae93bbbec325f
bitfl0wer <florian@pro-weber.com>
2023-11-18 20:02:50 +0100

    Add MessageCapable trait implementation for WsMessage

--- src/gateway/wasm/mod.rs
+++ src/gateway/wasm/mod.rs
@@ -3,3 +3,28 @@ pub mod heartbeat;
 use super::*;
 pub use gateway::*;
 pub use heartbeat::*;
+use ws_stream_wasm::WsMessage;
+
+impl crate::gateway::MessageCapable for WsMessage {
+    fn as_string(&self) -> Option<String> {
+        match self {
+            WsMessage::Text(text) => Some(text.clone()),
+            _ => None,
+        }
+    }
+
+    fn as_bytes(&self) -> Option<Vec<u8>> {
+        match self {
+            WsMessage::Binary(bytes) => Some(bytes.clone()),
+            _ => None,
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        match self {
+            WsMessage::Text(text) => text.is_empty(),
+            WsMessage::Binary(bytes) => bytes.is_empty(),
+            _ => false,
+        }
+    }
+}
