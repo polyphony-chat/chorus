37f3bcde940d732d3852967eddb2d811d1791ccf
bitfl0wer <florian@pro-weber.com>
2023-11-20 14:22:00 +0100

    Rename test function to generate_general_configuration_schema_wasm

--- tests/instance.rs
+++ tests/instance.rs
@@ -7,8 +7,8 @@ wasm_bindgen_test_configure!(run_in_browser);
 
 #[cfg(target_arch = "wasm32")]
 #[wasm_bindgen_test]
-async fn guild_creation_deletion_wasm() {
-    guild_creation_deletion()
+async fn generate_general_configuration_schema_wasm() {
+    generate_general_configuration_schema()
 }
 
 #[tokio::test]
