fcd86ad084a1f17e4abc5e2fa43b541c513fba64
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2023-12-16 21:56:04 +0100

    feat: Public api! (sorta)

--- src/voice/mod.rs
+++ src/voice/mod.rs
@@ -1,9 +1,9 @@
 //! Module for all voice functionality within chorus.
 
+mod crypto;
 pub mod gateway;
 pub mod udp;
 pub mod voice_data;
-mod crypto;
 
 // Pub use this so users can interact with packet types if they want
 pub use discortp;
