ef4d6cffdbb94ef2066f695c718fcb6a9665ff91
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2023-12-28 09:21:47 +0100

    feat: first try at vgw wasm compat

---
+++ src/voice/gateway/backends/tungstenite.rs
@@ -0,0 +1,65 @@
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    StreamExt,
+};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{
+    connect_async_tls_with_config, tungstenite, Connector, MaybeTlsStream, WebSocketStream,
+};
+
+use crate::{errors::VoiceGatewayError, voice::gateway::VoiceGatewayMesssage};
+
+#[derive(Debug, Clone)]
+pub struct TungsteniteBackend;
+
+// These could be made into inherent associated types when that's stabilized
+pub type TungsteniteSink =
+    SplitSink<WebSocketStream<MaybeTlsStream<TcpStream>>, tungstenite::Message>;
+pub type TungsteniteStream = SplitStream<WebSocketStream<MaybeTlsStream<TcpStream>>>;
+
+impl TungsteniteBackend {
+    pub async fn connect(
+        websocket_url: &str,
+    ) -> Result<(TungsteniteSink, TungsteniteStream), crate::errors::VoiceGatewayError> {
+        let mut roots = rustls::RootCertStore::empty();
+        for cert in rustls_native_certs::load_native_certs().expect("could not load platform certs")
+        {
+            roots.add(&rustls::Certificate(cert.0)).unwrap();
+        }
+        let (websocket_stream, _) = match connect_async_tls_with_config(
+            websocket_url,
+            None,
+            false,
+            Some(Connector::Rustls(
+                rustls::ClientConfig::builder()
+                    .with_safe_defaults()
+                    .with_root_certificates(roots)
+                    .with_no_client_auth()
+                    .into(),
+            )),
+        )
+        .await
+        {
+            Ok(websocket_stream) => websocket_stream,
+            Err(e) => {
+                return Err(VoiceGatewayError::CannotConnect {
+                    error: e.to_string(),
+                })
+            }
+        };
+
+        Ok(websocket_stream.split())
+    }
+}
+
+impl From<VoiceGatewayMesssage> for tungstenite::Message {
+    fn from(message: VoiceGatewayMesssage) -> Self {
+        Self::Text(message.0)
+    }
+}
+
+impl From<tungstenite::Message> for VoiceGatewayMesssage {
+    fn from(value: tungstenite::Message) -> Self {
+        Self(value.to_string())
+    }
+}
