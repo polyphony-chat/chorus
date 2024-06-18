d7de1d2fb7edd699a136ba01a9e494ff149e820b
Quat3rnion <quat3rnion@proton.me>
2024-06-18 05:46:27 -0400

    Fix compilation for real, no dirty hack

--- src/types/entities/invite.rs
+++ src/types/entities/invite.rs
@@ -86,7 +86,10 @@ impl From<Guild> for InviteGuild {
             premium_subscription_count: value.premium_subscription_count,
             nsfw_deprecated: None,
             nsfw_level: value.nsfw_level.unwrap_or_default(),
-            welcome_screen: (*value.welcome_screen).to_owned(), // Dirty hack to get around feature locks making different types
+            #[cfg(feature = "sqlx")]
+            welcome_screen: value.welcome_screen.0,
+            #[cfg(not(feature = "sqlx"))]
+            welcome_screen: value.welcome_screen,
         }
     }
 }
