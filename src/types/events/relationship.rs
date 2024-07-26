f386adc3eae14a49c8a20db34147fa63204d82e7
kozabrada123 <natantriestoedit@gmail.com>
2024-07-26 09:56:03 +0200

    Fix #525

--- src/types/events/relationship.rs
+++ src/types/events/relationship.rs
@@ -5,7 +5,7 @@
 use crate::types::{events::WebSocketEvent, Relationship, RelationshipType, Snowflake};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Default, WebSocketEvent)]
+#[derive(Debug, Deserialize, Serialize, Default, Clone, WebSocketEvent)]
 /// See <https://github.com/spacebarchat/server/issues/204>
 pub struct RelationshipAdd {
     #[serde(flatten)]
