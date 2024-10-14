6262d1782b4cf3f05bd3b7acd0d0a25120f273cd
bitfl0wer <florian@pro-weber.com>
2024-10-14 14:21:38 +0200

    allow deprecated

--- src/gateway/mod.rs
+++ src/gateway/mod.rs
@@ -2,6 +2,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#![allow(deprecated)] // Since Opcode variants marked as deprecated are being used here, we need to suppress the warnings about them being deprecated
+
 pub mod backends;
 pub mod events;
 pub mod gateway;
