85e922bc5039bd2af091b4a95d4b7f7d82ade7e2
kozabrada123 <natantriestoedit@gmail.com>
2024-11-06 09:40:07 +0100

    feat: update RequestGuildMembers, allow multiple snowflakes
    Updates the RequsetGuildMembers gateway event, allows to query multiple user_ids and guild_ids using OneOrMoreSnowflakes type, update its documentation

--- src/types/events/request_members.rs
+++ src/types/events/request_members.rs
@@ -2,17 +2,43 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-use crate::types::{events::WebSocketEvent, Snowflake};
+use crate::types::{events::WebSocketEvent, OneOrMoreSnowflakes};
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize, Default, WebSocketEvent, Clone)]
-/// See <https://discord.com/developers/docs/topics/gateway-events#request-guild-members-request-guild-members-structure>
+#[derive(Debug, Deserialize, Serialize, WebSocketEvent, Clone)]
+/// Used to request members for a guild or a list of guilds.
+///
+/// Fires multiple [crate::types::events::GuildMembersChunk] events (each with up to 1000 members)
+/// until all members that match the request have been sent.
+///
+/// # Notes
+/// One of `query` or `user_ids` is required.
+///
+/// If `query` is set, `limit` is required (if requesting all members, set `limit` to 0)
+///
+/// # Reference
+/// See <https://docs.discord.sex/topics/gateway-events#request-guild-members>
 pub struct GatewayRequestGuildMembers {
-    pub guild_id: Snowflake,
+	 /// Id(s) of the guild(s) to get members for
+    pub guild_id: OneOrMoreSnowflakes,
+
+	 /// The user id(s) to request (0 - 100)
+    pub user_ids: Option<OneOrMoreSnowflakes>,
+
+	 /// String that the username / nickname starts with, or an empty string for all members
     pub query: Option<String>,
-    pub limit: u64,
+
+	 /// Maximum number of members to send matching the query (0 - 100)
+	 ///
+	 /// Must be 0 with an empty query
+    pub limit: u8,
+
+	 /// Whether to return the [Presence](crate::types::events::PresenceUpdate) of the matched
+	 /// members
     pub presences: Option<bool>,
-    // TODO: allow array
-    pub user_ids: Option<Snowflake>,
+
+	 /// Unique string to identify the received event for this specific request.
+	 ///
+	 /// Up to 32 bytes. If you send a longer nonce, it will be ignored
     pub nonce: Option<String>,
 }
