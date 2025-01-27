54817b64c20fb601bacf2f0ec2e3023e7efcc2fc
kozabrada123 <59031733+kozabrada123@users.noreply.github.com>
2025-01-27 19:18:20 +0100

    Instance default events API (#598)
    * allow cloning gateway Events
    
    * feat: add default gateway events to Instance object
    
    This allows users to add listeners to events that are received before they gain access to the ChorusUser object,
    such a Ready, when using the Instance account creation / login methods.

--- src/types/config/types/guild_configuration.rs
+++ src/types/config/types/guild_configuration.rs
@@ -132,6 +132,9 @@ pub enum GuildFeatures {
     CrossChannelReplies,
     IrcLikeCategoryNames,
     InvitesClosed,
+    /// Old feature, used only on spacebar
+    ThreeDayThreadArchive,
+    SevenDayThreadArchive,
 }
 
 #[derive(Debug, Clone, PartialEq, Default, Serialize, Deserialize, Eq, Hash)]
@@ -355,6 +358,8 @@ impl FromStr for GuildFeatures {
             "CROSS_CHANNEL_REPLIES" => Ok(GuildFeatures::CrossChannelReplies),
             "IRC_LIKE_CATEGORY_NAMES" => Ok(GuildFeatures::IrcLikeCategoryNames),
             "INVITES_CLOSED" => Ok(GuildFeatures::InvitesClosed),
+            "THREE_DAY_THREAD_ARCHIVE" => Ok(GuildFeatures::ThreeDayThreadArchive),
+            "SEVEN_DAY_THREAD_ARCHIVE" => Ok(GuildFeatures::SevenDayThreadArchive),
             _ => Err(Error::Guild(GuildError::InvalidGuildFeature)),
         }
     }
@@ -510,6 +515,8 @@ impl GuildFeatures {
             GuildFeatures::CrossChannelReplies => "CROSS_CHANNEL_REPLIES",
             GuildFeatures::IrcLikeCategoryNames => "IRC_LIKE_CATEGORY_NAMES",
             GuildFeatures::InvitesClosed => "INVITES_CLOSED",
+            GuildFeatures::ThreeDayThreadArchive => "THREE_DAY_THREAD_ARCHIVE",
+            GuildFeatures::SevenDayThreadArchive => "SEVEN_DAY_THREAD_ARCHIVE",
         }
     }
 }
