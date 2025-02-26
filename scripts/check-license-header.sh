e563e4599c4ea54e430650270c4ded45147dc1a7
bitfl0wer <florian@pro-weber.com>
2025-02-26 18:47:15 +0100

    add check-license-header script

---
+++ scripts/check-license-header.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Execute this from the project root:
+# $: ./scripts/check-license-header.sh
+npx github:viperproject/check-license-header#v2 check -c .github/license-check/config.json --path .
