ce26d8dce4482f0b9d02d143dceedacd8d6d375e
bitfl0wer <florian@pro-weber.com>
2025-04-06 16:37:09 +0200

    Update dependencies

--- chorus-postgres/src/lib.rs
+++ chorus-postgres/src/lib.rs
@@ -2,8 +2,4 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-use chorus::types::errors::Error;
-use chorus::types::utils::configuration::SymfoniaConfiguration;
-use sqlx::{Database, Executor, PgPool, Row, postgres::PgConnectOptions};
-
 pub mod entities;
