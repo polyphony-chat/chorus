e1742e4025705f9482243e11d1daacff9274645c
bitfl0wer <florian@pro-weber.com>
2025-04-06 17:06:52 +0200

    use email_address crate to parse email, return usererror instead of error

--- chorus/src/types/utils/email.rs
+++ chorus/src/types/utils/email.rs
@@ -2,25 +2,21 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+use std::str::FromStr;
+
 use regex::Regex;
 
 use crate::types::errors::{Error, UserError};
 
-lazy_static::lazy_static! {
-    // TODO(bitfl0wer): Could we just use <https://docs.rs/email_address/latest/email_address/> here?
-    static ref EMAIL_REGEX: Regex = Regex::new(r#"^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"#).unwrap();
-}
-
-pub fn adjust_email(email: &str) -> Result<String, Error> {
+pub fn adjust_email(email: &str) -> Result<String, UserError> {
     if email.is_empty() {
-        return Err(Error::User(UserError::InvalidEmail));
+        return Err(UserError::InvalidEmail);
     }
 
-    if !EMAIL_REGEX.is_match(email) {
-        return Err(Error::User(UserError::InvalidEmail));
-    }
+    let checked_email =
+        email_address::EmailAddress::from_str(email).map_err(|_| UserError::InvalidEmail)?;
 
     // TODO: check accounts with uncommon email domains
     // TODO: replace .dots and +alternatives -> Gmail Dot Trick https://support.google.com/mail/answer/7436150 and https://generator.email/blog/gmail-generator
-    Ok(email.to_string())
+    Ok(checked_email.to_string())
 }
